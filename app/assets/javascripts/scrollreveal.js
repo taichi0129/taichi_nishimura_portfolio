59a9d06f57ac56f370d4ec2d91bd68d0395fe9d3
taichi0129 <nishi.made.t.9210@gmail.com>
2020-06-15 11:28:29 +0900

    portfolio initial commit

---
+++ app/assets/javascripts/scrollreveal.js
@@ -0,0 +1,5 @@
+$(function(){
+  ScrollReveal().reveal('.plofiles', { duration: '2000', viewFactor: '0.2'});
+  ScrollReveal().reveal('.skills', { duration: '2000', viewFactor: '0.2'});
+});
+  
---
+++ app/assets/javascripts/top-page.js
@@ -0,0 +1,30 @@
+$(function(){
+  // #で始まるリンクをクリックしたら実行されます
+  $('a[href^="#"]').click(function() {
+    // スクロールの速度
+    var speed = 500; // ミリ秒で記述
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+});
+
+$(function() {
+  var pagetop = $('#page_top');
+  pagetop.hide();
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+          pagetop.fadeIn();
+      } else {
+          pagetop.fadeOut();
+      }
+  });
+  pagetop.click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 500); //0.5秒かけてトップへ移動
+      return false;
+  });
+});
